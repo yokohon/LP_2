adcb4cd635ec0a19fc09b7967871692a59457acd
honda.youko <kyogranatus@jcom.home.ne.jpp>
2024-10-15 21:11:20 +0900

    sp

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,13 +1,26 @@
-$('.carousel').slick({  
-    autoplay: false, // 自動再生
-    autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
-    dots: true, // ドットを表示
-    fade: true, // フェードで切り替え
-  });
+$('.carousel').slick({
+  autoplay: false, // 自動再生
+  autoplaySpeed: 5000, // 自動再生の速さ、単位はミリ秒
+  dots: true, // ドットを表示
+  fade: true, // フェードで切り替え
+  mobileFirst: true, //初期設定がモバイル
+  slidesToShow: 1,
+  slidesToScroll: 1,
+
+  responsive: [
+    {
+      breakpoint: 768, // 768px以上の画面サイズ
+      settings: {
+        slidesToShow: 3, // このサイズでの表示スライド数
+        slidesToScroll: 3,
+      }
+    }
+  ]
+});
 
 
-  $('.quistion-strt').on('click', function(e) {
-    var content = $(this).next();
-    content.slideToggle();
-  $(this).toggleClass("open");
-  });
+$('.question-strt').on('click', function (e) {
+  var content = $(this).next();
+  content.slideToggle();
+  $(this).toggleClass("open"); 
+});
